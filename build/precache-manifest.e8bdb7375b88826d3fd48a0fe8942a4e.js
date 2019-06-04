5fc2dea3e70bc727aa884898c73caa619e9a199f
Aisalkyn Dzhenalieva <aidzhenalieva@gmail.com>
2019-06-04 13:44:06 +0600

    Test

---
+++ build/precache-manifest.e8bdb7375b88826d3fd48a0fe8942a4e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54bc9a4ba4327343fa10ecb05ee528c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a89eeba2b223aaf2a29",
+    "url": "/static/css/2.0ac97ba3.chunk.css"
+  },
+  {
+    "revision": "ce0f8be0158c16799ad2",
+    "url": "/static/css/main.05d76920.chunk.css"
+  },
+  {
+    "revision": "7a89eeba2b223aaf2a29",
+    "url": "/static/js/2.7e191e0c.chunk.js"
+  },
+  {
+    "revision": "ce0f8be0158c16799ad2",
+    "url": "/static/js/main.1f2faccf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
